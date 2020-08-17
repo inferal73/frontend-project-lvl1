11cd0761379e31fb56ae37b2b8a3ae6c5ceaf4d9
Ivan <inferal73@gmail.com>
2020-08-17 18:45:35 +0400

    adds lint & github action

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
 export const run = () => {
-    console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
-}
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
+
+export default run;
